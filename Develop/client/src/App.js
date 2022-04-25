ea8e29a2c01828fccce95b5f352f63c3e2951817
casscalex <casscalex@outlook.com>
2022-04-24 23:04:29 -0500

    added client and graphql to API

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -4,9 +4,26 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
+
 function App() {
   return (
-    <Router>
+    <ApolloProvider clinet ={client}>
+          <Router>
       <>
         <Navbar />
         <Switch>
@@ -16,6 +33,8 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
+
   );
 }
 
